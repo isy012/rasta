7f4439c970b215641d08ec8ca9b943be384bf958
arielmetayel <arielmetayel@gmail.com>
2015-07-04 01:47:36 +0300

    ;ojkEFS

--- angular/app.js
+++ angular/app.js
@@ -1,16 +1,25 @@
 var app = angular.module("myApp",[]);
 app.controller('general',function($scope, $timeout){
 
-$scope.tasksList = [];
-$scope.nowTime = '...'
-$scope.add = function (savedTask){
-var date = new Date();
-$scope.nowTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	$scope.tasksList = [];
+	$scope.nowTime = '...'
+	$scope.add = function (savedTask){
+		var date = new Date();
+		$scope.nowTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		$scope.tasksList.push({name: savedTask,time: $scope.nowTime, logo: date});
+	}
 
-$scope.tasksList.push({name: savedTask,time: $scope.nowTime, logo: date});
-}
+	$scope.tasksList.remove = function ($index){
+		$scope.tasksList.splice($index,1);
+	} 
+	});
 
-});
 
+// 	1. live \ cache --V
+// 	2. local storage \ cookie --?
+// 	persistent storag:
+// 	3. local file system
+// 	4. online \ cloud \ server
 
-
+// 1.button\remove
+// 2.save in cookie
